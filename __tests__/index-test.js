4ddca12cbbbda151d8128fc1f3648f70a293ec8b
andrey <a.shelikhov@gmail.com>
2021-04-08 21:18:47 +0400

    init

---
+++ __tests__/index-test.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { create, act } from "react-test-renderer";
+import { View } from "react-native";
+
+import ValuePicker from "../src";
+
+const Items = Array.from(Array(10).keys());
+
+const rednerItem = (item) => (
+  <View style={{ width: 50 }}>
+      { item }
+  </View>
+);
+
+const mockCallback = jest.fn(x => x);
+
+const tree = create(<ValuePicker
+  data={Items}
+  renderItem={rednerItem}
+  itemWidth={50}
+  selectedValue={1}
+  onChange={mockCallback}
+/>).toJSON();
+
+
+it(`renders correctly`, () => {
+  expect(tree).toMatchSnapshot();
+});
+
+const slider = tree.children[1].children[0];
+
+it(`render all items`, () => {
+  expect(slider.children.length).toBe(10);
+});
+
+act(() => {
+  slider.children[3].children[0].props.onClick();
+});
+
+it(`onChange calls by click`, () => {
+  expect(mockCallback.mock.calls.length).toBe(1);
+});
+
+it(`onChange return right value`, () => {
+  expect(mockCallback.mock.calls[0][0]).toBe(3);
+});
---
+++ src/index.tsx
@@ -0,0 +1,156 @@
+import React, {ReactNode, useState} from 'react';
+import {
+  View,
+  Text,
+  ScrollViewProps,
+  TouchableWithoutFeedback,
+  LayoutChangeEvent,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  Animated
+} from 'react-native';
+
+
+export interface Props extends ScrollViewProps {
+  style: object,
+  data: any[],
+  selectedValue: number,
+  renderItem: (item: any, index: number) => ReactNode,
+  itemWidth: number,
+  initialValue: number,
+  snapTimeout?: number,
+  onChange?: (position: number) => void,
+  mark?: (position: number) => ReactNode,
+  interpolateScale?: (index: number, itemWidth: number) => Animated.InterpolationConfigType,
+  interpolateOpacity?: (index: number, itemWidth: number) => Animated.InterpolationConfigType
+}
+
+
+export default (props: Props) => {
+  const {
+    data,
+    renderItem,
+    itemWidth,
+    style = {},
+    ...passedProps
+  } = props;
+
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const flatListRef = React.useRef(null);
+  let [paddingSide, setPaddingSide] = useState(0);
+
+  const onLayoutScrollView = (e: LayoutChangeEvent) => {
+    const {width} = e.nativeEvent.layout;
+    const {itemWidth, onLayout, selectedValue} = props;
+    setPaddingSide((width - itemWidth) / 2);
+
+    if (onLayout != null) {
+      onLayout(e);
+    }
+    if (selectedValue) {
+      scrollToPosition(selectedValue);
+    }
+  }
+
+  const onMomentumScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const x = e.nativeEvent.contentOffset.x;
+    const selected = Math.round(x / itemWidth);
+    changePosition(selected);
+    scrollToPosition(selected);
+  }
+
+  const scrollToPosition = (position: number) => {
+    if (flatListRef && flatListRef.current) {
+      // @ts-ignore
+      flatListRef.current.scrollToIndex({animated: true, index: "" + position});
+    }
+  }
+
+  const changePosition = (position: number) => {
+    const {onChange} = props;
+    if (onChange != null) {
+      if (position < 1) {
+        onChange(0);
+      } else if (position > props.data.length) {
+        onChange(props.data.length - 1);
+      } else {
+        onChange(position);
+      }
+    }
+  }
+
+  return (
+    <View style={{display: "flex", height: "100%", ...style}} {...passedProps}>
+      <View style={{
+        position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'
+      }}>
+        {props.mark || DefaultMark()}
+      </View>
+      <Animated.FlatList
+        ref={process.env.NODE_ENV === 'test' ? null : flatListRef}
+        onLayout={onLayoutScrollView}
+        onScroll={Animated.event([{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: true})}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        data={data}
+        keyExtractor={(_item, index) => index.toString()}
+        onMomentumScrollEnd={onMomentumScrollEnd}
+        contentContainerStyle={{
+          paddingHorizontal: paddingSide, display: "flex", alignItems: "center", backgroundColor: 'transparent'
+        }}
+        renderItem={({item, index}) => {
+          const {itemWidth, interpolateScale, interpolateOpacity} = props;
+
+          const scale = scrollX.interpolate(interpolateScale ?
+            interpolateScale(index, itemWidth) :
+            defaultScaleConfig(index, itemWidth));
+
+          const opacity = scrollX.interpolate(interpolateOpacity ?
+            interpolateOpacity(index, itemWidth) :
+            defaultOpacityConfig(index, itemWidth));
+
+          return (
+            <TouchableWithoutFeedback onPress={() => {
+              changePosition(index);
+              scrollToPosition(index);
+            }} key={index}>
+              <Animated.View style={{transform: [{scale}], opacity}}>
+                {renderItem(item, index)}
+              </Animated.View>
+            </TouchableWithoutFeedback>
+          )
+        }}/>
+    </View>
+  );
+}
+
+const DefaultMark = () => (
+  <Text style={{
+    color: "black",
+    fontWeight: "bold",
+    paddingTop: 60
+  }}>^</Text>
+);
+
+const defaultScaleConfig = (index: number, itemWidth: number) => ({
+  inputRange: [
+    itemWidth * (index - 2),
+    itemWidth * (index - 1),
+    itemWidth * index,
+    itemWidth * (index + 1),
+    itemWidth * (index + 2),
+  ],
+  outputRange: [1, 1.5, 2.2, 1.5, 1]
+});
+
+const defaultOpacityConfig = (index: number, itemWidth: number) => ({
+  inputRange: [
+    itemWidth * (index - 2),
+    itemWidth * (index - 1),
+    itemWidth * index,
+    itemWidth * (index + 1),
+    itemWidth * (index + 2),
+  ],
+  outputRange: [0.7, 0.9, 1, 0.9, 0.7]
+});
